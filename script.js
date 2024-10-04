dabbfd1cff709c3370ad6ec24b8910392391240a
Nino-Devibar <devibarninonazlicudine@gmail.com>
2024-10-04 18:56:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 document.getElementById('knowMeBtn').addEventListener('click', function(event) {
     event.preventDefault();
-
-
     const letter = document.getElementById('firstLetter');
-    const button = this;
 
 
     letter.classList.add('animate');
 
 
     setTimeout(() => {
+        letter.style.opacity = 1;
         document.body.classList.add('fade-out');
     }, 3200);
 
 
     setTimeout(() => {
         window.location.href = "Netflix.html";
-    }, 3700);
+    }, 4000);
 });
